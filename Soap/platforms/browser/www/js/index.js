70bbb70d577c86ec9e93a9464bd1e41b868d7255
cuylerwarnock <cuyler.warnock@bobcats.gcsu.edu>
2016-06-15 15:29:42 -0400

    Commit on 6/15/16

--- Soap/platforms/browser/www/js/index.js
+++ Soap/platforms/browser/www/js/index.js
@@ -2,13 +2,14 @@ window.onload = function () {
     document.addEventListener("deviceready", init, false);
 
     init();
+
 }
 
 function init() {
     document.getElementById('btnJoke').addEventListener('click', getJoke, false);
     document.getElementById('btnQuery').addEventListener('click', getNewJoke, false);
     document.getElementById('btnZip').addEventListener('click', getWeather, false);
-    //document.getElementById('zip').addEventListener('click',getWeather,false);
+    getTrainData(); //document.getElementById('zip').addEventListener('click',getWeather,false);
 }
 // ------------Joke---------------- 
 function getJoke() {
@@ -68,4 +69,49 @@ function showWeather(xml) {
     output += "Description: " + description + "<br />";
 
     document.getElementById("resultWeather").innerHTML = output;
+}
+
+function getTrainData() {
+    $.ajax({
+        type: "GET",
+        url: "http://www3.septa.org/hackathon/Arrivals/90404/10",
+        dataType: "text",
+        success: function (result) {
+            showTrainData(result);
+
+        }
+    });
+}
+
+function showTrainData(result) {
+    var data = jQuery.parseJSON(result);
+    var arr = data[Object.keys(data)];
+    var northbound = arr[0].Northbound;
+    var southbound = arr[1].Southbound;
+
+
+    var output = "Northbound<div class='table'>";
+    for (i = 0; i < northbound.length; i++) {
+        output += "<span class='row'>";
+        output += "<span class='cell train_id border'>" + northbound[i].train_id + "</span>";
+        output += "<span class='cell depart_time border'>" + northbound[i].depart_time + "</span>";
+        output += "<span class='cell destination border'>" + northbound[i].destination + "</span>";
+        output += "</span>";
+    }
+    output += "</div>";
+    output += "<br /><br />";
+
+    output += "Southbound<div class='table'>";
+    for (i = 0; i < southbound.length; i++) {
+        output += "<span class='row'>";
+        output += "<span class='cell train_id border'>" + southbound[i].train_id + "</span>";
+        output += "<span class='cell depart_time border'>" + southbound[i].depart_time + "</span>";
+        output += "<span class='cell destination border'>" + southbound[i].destination + "</span>";
+        output += "</span>";
+    }
+    output += "</div>";
+
+
+
+    document.getElementById("trainTables").innerHTML = output;
 }
